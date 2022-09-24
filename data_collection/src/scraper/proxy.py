88f06a0abe6b3132d33f511cf8e4e1179bab7df3
Thamognya Kodi <contact@thamognya.com>
2022-09-24 23:20:13 +0700

    update: fix proxies later

--- data_collection/src/scraper/proxy.py
+++ data_collection/src/scraper/proxy.py
@@ -1,12 +1,15 @@
+from this import d
 from proxy_randomizer import RegisteredProviders
 import requests
 
 rp = RegisteredProviders()
 rp.parse_providers()
 
-class ProxyList:
+class ProxyClass:
 
-    def getProxies(self, n):
-        proxies = []
-        proxies.append(rp.get_random_proxy())
-        return proxies
+    def getProxy(self):
+        proxy = rp.get_random_proxy()
+        return [proxy.ip_address, proxy.port, proxy.ip_address + ":" + proxy.port]
+
+proxy = ProxyClass()
+print(proxy.getProxy())
--- data_collection/src/scraper/scrape.py
+++ data_collection/src/scraper/scrape.py
@@ -1,9 +1,9 @@
-from lib2to3.pgen2 import driver
-from lib2to3.pgen2.token import OP
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
-import re
+from selenium.webdriver.common.proxy import Proxy, ProxyType
+from selenium.webdriver.firefox.options import DesiredCapabilities
+import proxy
 
 """
 Resources:
@@ -14,31 +14,50 @@ Resources:
 opts = Options()
 opts.set_preference("browser.download.dir", "/home/jeff/Documents/gitRepos/PlotBERT/data/")
 opts.headless = True
-driver = webdriver.Firefox(options=opts)
+
+proxyClass = proxy.ProxyClass()
 
 class ScrapeClass:
 
-    def __init__(self, n = 0, url=""):
+    def __init__(self, n = 0, url="", linkOfBooks = []):
         self.n = n
         self.url = url
+        self.linkOfBooks = linkOfBooks
     
     def setNumberOfBooks(self, n):
         self.n = n
 
-    def getZLibUrl(self):
+    def getZLibUrl(self, driver):
         driver.get("https://z-lib.org")
         redirectedLink = driver.find_element(By.CSS_SELECTOR, "a.domain-check-link").get_attribute("href")
         self.url = redirectedLink
         return redirectedLink
 
+    def downloadBooks(self, url):
+        return "hello"
+
     def openZLibUrl(self):
-        self.getZLibUrl()
-        driver.get(self.url)
-        arraysOfBooks = driver.find_elements(By.CSS_SELECTOR, "a.book-loading")
-        linkOfBooks = []
-        for book in arraysOfBooks:
-            linkOfBooks.append(book.get_attribute("href"))
-        return linkOfBooks
+        firefox_capabilities = DesiredCapabilities().FIREFOX
+        for i in range(self.n):
+            if (i % 5 == 0):
+                myProxy = proxyClass.getProxy()
+                firefox_proxies = Proxy()
+                firefox_proxies.ssl_proxy = myProxy[2]
+                firefox_proxies.add_to_capabilities(firefox_capabilities)
+            # TODO: add proxy to webdriver somehow
+            print(myProxy)
+            driver = webdriver.Firefox(options=opts, desired_capabilities=firefox_capabilities)
+            self.getZLibUrl(driver)
+            driver.get(self.url)
+            arraysOfBooks = driver.find_elements(By.CSS_SELECTOR, "a.book-loading")
+            i = 0
+            for book in arraysOfBooks:
+                self.linkOfBooks.append(book.get_attribute("href"))
+                i += 1
+                if (i == 5): 
+                    break
+        return self.linkOfBooks
 
 scrapeTest = ScrapeClass()
+scrapeTest.setNumberOfBooks(1)
 print(scrapeTest.openZLibUrl())
